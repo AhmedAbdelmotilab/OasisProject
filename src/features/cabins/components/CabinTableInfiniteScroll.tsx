95573cfbec0f9f8c062852da8196dd918d761ffe
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2026-08-17 12:26:39 +0300

    feat: add responsive add cabin button and optimize layout for mobile screens

--- src/features/cabins/components/CabinTableInfiniteScroll.tsx
+++ src/features/cabins/components/CabinTableInfiniteScroll.tsx
@@ -46,6 +46,15 @@ export default function CabinTablePaginated() {
 
   return (
     <div className={styles.table}>
+      <button
+        className={styles.mobileAddButton}
+        onClick={() => {
+          setShowOpenModal();
+          setEditingCabinId(null);
+        }}
+      >
+        <FaPlus /> {!showOpenModal ? "Add" : "close"}
+      </button>
       <header className={styles.header}>
         <div>Image</div>
         <div>Cabin</div>
