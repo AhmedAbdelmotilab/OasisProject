a40471cbccd7873c78d388f4d10b5e8aa43bb42d
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2026-08-19 12:10:32 +0300

    Localize cabins page heading
    This change adds the i18n hook to the Cabins page and translates the page title using react-i18next instead of a hardcoded English string. This keeps the UI consistent with the app's localization support.

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,14 +1,17 @@
+import { useTranslation } from "react-i18next";
 import AddCabin from "../features/cabins/components/AddCabin";
 import CabinTablePaginated from "../features/cabins/components/CabinTableInfiniteScroll";
 import CabinTableOperations from "../features/cabins/components/CabinTableOperations";
 import styles from "./Cabins.module.css";
 
 function Cabins() {
+  const { t } = useTranslation();
+
   return (
     <>
       <AddCabin />
       <div className={styles.horizontal}>
-        <h1 className={`${styles.heading} ${styles.h1}`}>All cabins</h1>
+        <h1 className={`${styles.heading} ${styles.h1}`}>{t("All cabins")}</h1>
         <CabinTableOperations />
       </div>
       <CabinTablePaginated />
